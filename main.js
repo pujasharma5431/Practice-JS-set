db248d025b12a78053d57f6f93a6a426b7d202a2
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-01 14:08:43 +0545

    QA

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let a="Puja Sharma"
 let b= a + 6;
 console.log(b);
 
-// Q2. Use type of operator to find the datatype of string in last question
+// // Q2. Use type of operator to find the datatype of string in last question
 
 console.log(typeof(b));
 
-// Q3 Create a const object in javascript. Can you change it to hold a number later?
+// // Q3 Create a const object in javascript. Can you change it to hold a number later?
 
 const pi={
     pin:3.14,
@@ -17,8 +17,20 @@ const pi={
 // pi=9.2;[No we cannnot]
 // console.log(pi)
 
-// Q4 Try to add a new key to the const object in Problem 3 were you able to do it?
+// // Q4 Try to add a new key to the const object in Problem 3 were you able to do it?
 
 pi['name']='AG'
 
 console.log(pi)
+
+// Q5 Write a JS program to create a word meaning dictionary of 5 words.
+
+const dictionary={
+    appreciate:"ackknowledge",
+    Famous:"nown",
+    Faous:"Knon",
+    Fams:"Knn",
+    Famo:"K"
+}
+console.log(dictionary["Faous"])
+
