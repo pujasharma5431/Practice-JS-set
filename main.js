75526c0d1d29225ce37826af9a167c410dde242a
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-01 12:52:29 +0545

    Q3

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ console.log(b);
 // Q2. Use type of operator to find the datatype of string in last question
 
 console.log(typeof(b));
+
+// Q3 Create a const object in javascript. Can you change it to hold a number later?
+
+const pi=3.14;
+// pi=9.2;[No we cannnot]
+console.log(pi)
+
+// Q4 Try to add a new key to the const object in Problem 3 were you able to do it?
