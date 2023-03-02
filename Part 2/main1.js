931dbbe4e4ec58d63cda3e42b65b2c358220baea
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-02 10:44:22 +0545

    Q2

--- Part 2/main1.js
+++ Part 2/main1.js
@@ -1,10 +1,34 @@
 // Q1. Use logical operators to find wheather the age of a person lies betweeen 10 and 20?
 
-let age= prompt("Enter the age of a Person")
-    if(age>=10 && age<=20)
-        console.log("valid")
+// let age= prompt("Enter the age of a Person")
+//     if(age>=10 && age<=20)
+//         console.log("valid")
 
 
-else {
-    console.log("Invalid")
+// else {
+//     console.log("Invalid")
+// }
+
+// Q2. Demostarate the use of Switch Case Statements in JavaScript
+
+let day =prompt("Enter the day");
+day=Number.parseInt(day)1
+switch(day){
+    case 1:
+        console.log("1st day is Sunday")
+        break;
+        case 2:
+            console.log("Monday")
+            break;
+          
+            case 3:
+                console.log("Tuesday")
+                break;1
+               
+
+                default:
+                    console.log("Doesn't exist")
+
+
+
 }
