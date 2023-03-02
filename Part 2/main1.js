2645bbaa42cf3fe377c05dd6774b64a4f2cab034
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-02 10:23:41 +0545

    Practice set 2

---
+++ Part 2/main1.js
@@ -0,0 +1,10 @@
+// Q1. Use logical operators to find wheather the age of a person lies betweeen 10 and 20?
+
+let age= prompt("Enter the age of a Person")
+    if(age>=10 && age<=20)
+        console.log("valid")
+
+
+else {
+    console.log("Invalid")
+}
