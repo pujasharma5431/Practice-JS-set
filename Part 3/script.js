482a2f5066a06647d072b193ddefa685111ef74f
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-03 09:41:20 +0545

    Q1

--- Part 3/script.js
+++ Part 3/script.js
@@ -6,4 +6,5 @@ let marks={
     Prince: 62
 }
 for (i=0; i<Object.keys(marks).length; i++){
-console.log(Object.keys(marks)[i]+ marks[Object.keys(marks)[i]])}
+console.log(Object.keys(marks)[i]+ marks[Object.keys(marks)[i]])
+}
