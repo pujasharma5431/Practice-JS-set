bbfddfb16f95c09812f799aafe2b49745503dc6f
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-10 14:45:58 +0545

    part 4

--- Part 4/scri.js
+++ Part 4/scri.js
@@ -2,3 +2,10 @@
 
 const a = "Puja Sharma";
 console.log(a.toUpperCase())
+
+// Q2. Try to change 4th charcter of a given string were you able to do it?
+
+let fruit ="Apple";
+fruit[4]="o";
+console.log(fruit)
+
---
+++ Part 5/scr.js
@@ -0,0 +1,9 @@
+  
+// Q1. Create an array of numbers and take input from the user to add numbers to this array
+
+
+let array=[1,2,3,4,5];
+let a= prompt("Enter a number");
+a=Number.parseInt(a);
+array.push(a);
+console.log(array);
