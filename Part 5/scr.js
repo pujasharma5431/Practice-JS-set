22d86f51342d19b034883e5a96952aaf011e2700
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-10 15:20:12 +0545

    Q3

--- Part 5/scr.js
+++ Part 5/scr.js
@@ -11,13 +11,23 @@
 // Q2. Keep adding numbers to the array in Q1 until 0 is added to the array
 
 
-let array=[1,2,3,4,5];
-let a;
-do{
-    a= prompt("Enter a number");
-a=Number.parseInt(a);
-array.push(a);
-console.log(array);
+// let array=[1,2,3,4,5];
+// let a;
+// do{
+//     a= prompt("Enter a number");
+// a=Number.parseInt(a);
+// array.push(a);
+// console.log(array);
 
-}
-while(a!=0);
+// }
+// while(a!=0);
+
+
+
+// Q3. filter for numbers divisible by 10 from a given array
+
+let num=[2,5,4,10,15];
+let num1=num.filter((values)=>{
+    return values%10 ==0;
+})
+console.log(num1)
