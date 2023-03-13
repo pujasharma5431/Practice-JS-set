5df2364d112e43457c9f238118033c1e21ebc6d8
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-13 13:54:10 +0545

    Q1

--- Part 6/sscript.js
+++ Part 6/sscript.js
@@ -2,11 +2,25 @@
 // <!-- Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive -->
 
 
-let age=prompt("Enter your age");
-age=Number.parseInt(age)
-if(age>=18){
+// let age=prompt("Enter your age");
+// age=Number.parseInt(age)
+// if(age>=18){
+//     alert("You can drive")
+// }
+// else{
+//     alert("You cannot drive")
+// }
+
+let age=prompt("Enter your age")
+age=Number.parseInt(age);
+const canDrive=(age)=>{
+    age>=18;
+}
+if(canDrive(age)){
     alert("You can drive")
 }
 else{
     alert("You cannot drive")
 }
+
+
