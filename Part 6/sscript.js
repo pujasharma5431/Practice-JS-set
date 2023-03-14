b5aaac04af6803c4064a9c5648e42ef8760ef42f
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-14 08:42:33 +0545

    Q2

--- Part 6/sscript.js
+++ Part 6/sscript.js
@@ -11,16 +11,41 @@
 //     alert("You cannot drive")
 // }
 
-let age=prompt("Enter your age")
-age=Number.parseInt(age);
-const canDrive=(age)=>{
-    age>=18;
+// let age=prompt("Enter your age")
+// age=Number.parseInt(age);
+// const canDrive=(age)=>{
+//     age>=18;
+// }
+// if(canDrive(age)){
+//     alert("You can drive")
+// }
+// else{
+//     alert("You cannot drive")
+// }
+
+
+// Q2. If Q1 use confirm to ask the user if he wants to see the prompt again
+
+
+let age1=prompt("Enter your age");
+age1=Number.parseInt(age1);
+let runAgain=true;
+
+while(runAgain){
+
+
+
+const canDrive=(age1)=>{
+return (age1>=18)
 }
-if(canDrive(age)){
+
+
+if(canDrive(age1)){
     alert("You can drive")
 }
 else{
     alert("You cannot drive")
 }
 
-
+runAgain=confirm("You want to run again")
+}
